510e06c76cf5c5c3d37a74bc95e618a31fba67d9
伊藤純一 <itoujunichi@macbook-pc1087.local>
2017-09-28 20:05:01 +0900

    Initial Commit

---
+++ TopViewSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TopViewSample
+//
+//  Created by 伊藤純一 on 2017/09/28.
+//  Copyright © 2017年 ji. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
