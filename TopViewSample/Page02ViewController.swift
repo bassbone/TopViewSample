62bb6adb3d9c2ee008d642bb724686504766446e
伊藤純一 <itoujunichi@macbook-pc1087.local>
2017-10-02 20:12:03 +0900

    ボタンを押した後の挙動を追加

--- TopViewSample/Page02ViewController.swift
+++ TopViewSample/Page02ViewController.swift
@@ -23,15 +23,30 @@ class Page02ViewController: UIViewController {
         for index in 0...7 {
             let subContentView = UIView(frame: CGRect(x: size.width * CGFloat(index) * 0.5, y: 0, width: size.width * 0.5, height: size.height))
             subContentView.backgroundColor = UIColor.init(red: CGFloat(0.125 * Double(index)), green: CGFloat(0.125 * Double(index)), blue: CGFloat(0.125 * Double(index)), alpha: 1)
+            subContentView.isUserInteractionEnabled = true
+            subContentView.tag = index
+            subContentView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(self.viewTapped(_:))))
             contentView.addSubview(subContentView)
         }
         
         scrollView.addSubview(contentView)
         scrollView.contentSize = contentView.frame.size
-        scrollView.contentOffset = CGPoint(x: size.width, y: 0)
+        scrollView.contentOffset = CGPoint(x: 0, y: 0)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+    }
+    
+    @objc func viewTapped(_ sender: UITapGestureRecognizer) {
+        let alertController = UIAlertController(title: "メッセージ", message: String(sender.view!.tag) + "番目のボタンが押されました！", preferredStyle: UIAlertControllerStyle.alert)
+        let okAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.default){ (action: UIAlertAction) in
+            // 実行させたい処理
+        }
+        let cancelButton = UIAlertAction(title: "CANCEL", style: UIAlertActionStyle.cancel, handler: nil)
+        alertController.addAction(okAction)
+        alertController.addAction(cancelButton)
+        
+        present(alertController, animated: true, completion: nil)
     }
 }
